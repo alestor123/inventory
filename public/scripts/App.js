bf8522d7c5a93d1edbcd71d6420cdc12f0de672d
Alestor Aldous <alestoraldous@gmail.com>
2024-11-05 19:12:40 +0530

    added search

--- public/scripts/App.js
+++ public/scripts/App.js
@@ -1,37 +1,37 @@
-const searchInput = document.querySelector("searchBarInput")
+const searchInput = document.querySelector(".searchBarInput")
 const list = document.querySelector('.list');
 
-const main = async () => {
-    const {username} = (await (await fetch("./config/user.json")).json());
-    const data = (await (await fetch(`https://api.github.com/users/${username}`)).json());
-    setProfile(data);
-};
-main();
+// const main = async () => {
+//     const {username} = (await (await fetch("./config/user.json")).json());
+//     const data = (await (await fetch(`https://api.github.com/users/${username}`)).json());
+//     setProfile(data);
+// };
+// main();
 
 
-function setProfile({bio,blog,avatar_url,followers,public_repos,name,company}) {
-document.querySelector('.info').innerHTML = `
-<figure>
-            <img alt="User Profile" src="${avatar_url}" />
-        </figure>
-        <div>
-            <h2><a href=${blog}><strong>${name}</strong></a></h2>
-            <p>${bio}</p>
-            <p>
-                Followers: <strong>${followers}</strong>
-                Repos: <strong>${public_repos}</strong>
-                ${company ? "Work: "+company : "" }
+// function setProfile({bio,blog,avatar_url,followers,public_repos,name,company}) {
+// document.querySelector('.info').innerHTML = `
+// <figure>
+//             <img alt="User Profile" src="${avatar_url}" />
+//         </figure>
+//         <div>
+//             <h2><a href=${blog}><strong>${name}</strong></a></h2>
+//             <p>${bio}</p>
+//             <p>
+//                 Followers: <strong>${followers}</strong>
+//                 Repos: <strong>${public_repos}</strong>
+//                 ${company ? "Work: "+company : "" }
 
-            </p>
+//             </p>
             
-        </div>
-        `;
-}
+//         </div>
+//         `;
+// }
 
 
 async function displayDirs(dirs) {
-        const {items} = (await (await fetch("./config/user.json")).json());
-
+const {items} = (await (await fetch("./config/user.json")).json());
+searchInput.classList.remove('hide');
 items.forEach(dir => {
          let liItem = document.createElement('li');
          liItem.classList.add('itm');
@@ -50,3 +50,17 @@ items.forEach(dir => {
 });    // }
 }
 displayDirs()
+
+// SEARCH FEAT 
+
+
+searchInput.addEventListener('input', (e) => {
+    const search = e.target.value;
+    const items = document.querySelectorAll('.itm');
+    const searchNormalize = search.toLowerCase();
+    items.forEach((item) => {
+        const normalText = item.innerText.toLowerCase();
+        if (normalText.includes(searchNormalize)) item.classList.remove('remove');
+        else item.classList.add('remove');
+    });
+});
