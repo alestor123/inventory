a00114e08750e28dbff086fe11b6508a021bcf51
Alestor Aldous <alestoraldous@gmail.com>
2024-11-08 19:56:51 +0530

    added empty page

--- public/scripts/Route.js
+++ public/scripts/Route.js
@@ -1,16 +1,24 @@
-var searchParams = (new URLSearchParams(window.location.search))
-var type = searchParams.get('type')
-console.log(type)
+const searchParams = (new URLSearchParams(window.location.search))
+const type = searchParams.get('type')
+const maintitle = document.getElementById('main-title')
+const maindesc = document.getElementById('main-description')
 
 
 async function run() {
-const {items} = (await (await fetch("./config/user.json")).json());
-if(!items.some(keytype => keytype.type === type)) window.location.replace("/404.html");
+const {items,contents} = (await (await fetch("./config/user.json")).json());
+// code 404
+const isFound = keytype => (keytype.type === type)
+if(!items.some(isFound)) window.location.replace("/404.html");
+const {title,description} = items.find(isFound)
+const fileteredList = contents.filter(contentType => contentType.type === type)
+if(fileteredList.length===0) window.location.replace("/empty.html");
+maintitle.innerHTML = title
+maindesc.innerHTML = description
 
 }
 
-async function fetchData() {
+// async function fetchData() {
     
 
-}
+// }
 run()
