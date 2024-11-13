da7d98fe2bee0bf7be269e58b1469c57f80f1a44
Alestor Aldous <alestoraldous@gmail.com>
2024-11-13 18:40:04 +0530

    added media including gallery

--- public/scripts/popup.js
+++ public/scripts/popup.js
@@ -18,7 +18,55 @@ popupWindow.innerHTML = `
       
       <!-- Video container -->
       <div class="video-container">
-${data.demoVideo ? `<iframe title="vimeo-player" src="${data.demoVideo}"  frameborder="0"  id="project-video"  allowfullscreen></iframe>` : " <h2>NO MEDIA AVAILABLE</h2>"}
+${data.demoVideo ? `<iframe title="vimeo-player" src="${data.demoVideo}"  frameborder="0"  id="project-video"  allowfullscreen></iframe>` : "" } 
+${data.demoImage && data.demoImage.constructor==Array && data.demoImage.length>=1 ? `
+  
+${data.demoImage.length==1  ? `<img src="${data.demoImage}" alt="DEMO IMAGE" width="500" height="300">`: 
+`
+
+ <div class="slideshow-container">
+${
+ data.demoImage.map((e,num) => { 
+return `<div class="mySlides fade">
+                <div class="numbertext">${num+1}/${data.demoImage.length}</div>
+                <img src="${e}" width="500" height="300">
+              </div>`
+  
+  
+
+  }).join('')
+
+}            
+              <button class="prev" onclick="plusSlides(-1)">❮</button>
+              <button class="next" onclick="plusSlides(1)">❯</button>
+              
+              </div>
+              <br>
+              
+              <div style="text-align:center">
+
+
+${
+ data.demoImage.map((e,num) => { 
+return `<span class="dot" onclick="currentSlide(${num+1})"></span>`
+  
+  
+
+  }).join('')
+
+}            
+</div>
+              </div>
+
+
+`} 
+  
+  
+  
+  ` : ""}
+
+${!data.demoVideo && !data.demoImage? `<h2>Kitty says: Media not found</h2> <img src ='https://media.tenor.com/J7GBdx1T2q4AAAAi/pusheen-detective.gif' id='media-not-found' >` : ""}
+
             </div>
       
       <!-- Video info -->
@@ -47,6 +95,7 @@ ${data.demoVideo ? `<iframe title="vimeo-player" src="${data.demoVideo}"  frameb
 </div>
 
 `
+
 const popup = document.getElementById('popup');
  const close = document.querySelector('.close');
  const tagsList = document.getElementById('tags-list');
@@ -89,4 +138,34 @@ const popup = document.getElementById('popup');
    tagElement.textContent = tag;
    tagsList.appendChild(tagElement);
  });
+ if(data.demoImage && data.demoImage.constructor==Array && data.demoImage.length>=1) plusSlides(1)
+
+
 }
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
